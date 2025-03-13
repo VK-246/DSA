7a00b3fb85e3686bf57a8a483cd99d9ce6f2dd87
VK-246 <vinayakkanhegaonkar5383@gmail.com>
2025-03-13 22:46:26 +0530

    Time: 161 ms (9.05%), Space: 11.8 MB (58.26%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // using hashmap -> store elements until dulicate is encountered
-        int maxi = 0;
         int n = s.size();
-        if(n == 1){ return 1;}
-        cout<<n;
-        for( int i = 0; i<n; i++){
-            map<char,int> mpp;
-            int cnt = 0;
-            for( int j = i; j<n; j++){
-                if( mpp.find(s[j]) == mpp.end() ){
-                    mpp[s[j]]++;
-                    cnt++;
-                    maxi = max(maxi,cnt);
-                }
-                else{
-                    maxi = max(maxi,cnt);
-                    // cnt = 1;
-                    break;
-                }
+        unordered_map<char,int> mpp; // stores char and index
+        int l = 0,r = 0;
+        int maxi = 0;
+        while( r < n ){
+            // if( )
+            if( mpp.find(s[r]) == mpp.end() || mpp[s[r]] < l ){
+                mpp[s[r]] = r;
             }
+            else{
+                l = mpp[s[r]]+1;
+                mpp[s[r]] = r;
+            }
+            // update maxi
+            maxi = max( maxi , r-l+1);
+            cout<<maxi<<" ";
+            r++;
         }
-            return maxi;    
+        return maxi;
+
 }
 };
