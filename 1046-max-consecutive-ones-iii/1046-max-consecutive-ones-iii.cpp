64787cca64fb1e93b851a99a3b06766220bba23d
VK-246 <vinayakkanhegaonkar5383@gmail.com>
2025-03-14 02:06:13 +0530

    Time: 0 ms (100%), Space: 59.2 MB (59.32%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -5,23 +5,41 @@ public:
         int start = 0, end = 0;
         int cnt0 = 0;
         int maxi = 0;
-        while( end < n ){
-            if( nums[end] == 0 ){
+        // Optimized 1
+        // while( end < n ){
+        //     if( nums[end] == 0 ){
+        //         cnt0++;
+        //     }
+        //     if( cnt0 > k ){
+        //         while( cnt0 > k ){
+        //             if( nums[start] == 0 ){
+        //                 cnt0--;
+        //             }
+        //             start++;
+        //         }
+        //     }
+        //     else{
+        //         maxi = max( maxi , end - start + 1);
+        //     }
+        //     end++;
+        // }
+
+
+        // OPtimized 2
+        while( end < n){
+            if( nums[end] == 0){
                 cnt0++;
             }
-            if( cnt0 > k ){
-                while( cnt0 > k ){
-                    if( nums[start] == 0 ){
-                        cnt0--;
-                    }
-                    start++;
-                }
+            if( cnt0 > k){
+                if(nums[start] == 0){cnt0--;}
+                start++;
             }
-            else{
-                maxi = max( maxi , end - start + 1);
+            if( cnt0 <= k ){
+                maxi = max( maxi , end - start + 1 );
             }
             end++;
         }
+
         return maxi;
     }
 };
